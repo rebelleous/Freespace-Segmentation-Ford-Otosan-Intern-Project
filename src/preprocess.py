d0713470f67979130020fa60872dec0ecd2611c1
Okan Dogan <56580536+rebelleous@users.noreply.github.com>
2020-09-12 18:30:46 +0300

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,34 @@
 
+from cv2 import cv2
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
+
+
+def tensorize_image(image_path, output_shape):
+    pass
+
+
+
+
+
+
+tensorize_mask [batch_size, width, height]
+
+encoder = OneHotEncoder()
+encoder.fit(copy_mask)
+
+torch.Tensor()
+
+batch_masks = []
+
+batch_masks.append(copy_mask)
+
+batch_masks = np.array(batch_masks)
+
+mask_tensor = torch.Tensor(batch_masks)
+
+print(mask_tensor.shape)
+
+
+
