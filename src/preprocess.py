3e86583f030cceca465dd78305d435abe462fdb1
Okan Dogan <56580536+rebelleous@users.noreply.github.com>
2020-10-17 18:02:52 +0300

    corr final

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,61 +1,93 @@
 from cv2 import cv2
+import torch
 import numpy as np
-import os
-import copy
-import argparse
-from sklearn.preprocessing import OneHotEncoder
+import glob
 
-IMAGE_DIR  = '../data/images'
-all_image = sorted(os.listdir(image_dir))
+def tensorize_image(image_path_list, output_shape, cuda=False):
+    image_list = []
+    for image_path in image_path_list:
+        image = cv2.imread(image_path)
+        image = cv2.resize(image, output_shape)
+        torchlike_image = torchlike_data(image)
+        image_list.append(torchlike_image)
+        
+    image_array = np.array(image_list, dtype=np.float32)
+    torch_image = torch.from_numpy(image_array).float()
+    if cuda:
+        torch_image = torch_image.cuda()
+    return torch_image
 
-for f in all_image:
-    image_file = image_dir + f.replace('jpg', 'png')
+def tensorize_mask(mask_path_list, output_shape, n_class, cuda=False): 
+    mask_list = []
+    for mask_path in mask_path_list:
+        mask = cv2.imread(mask_path, 0)
+        mask = cv2.resize(mask, output_shape)
+        mask = one_hot_encoder(mask, n_class)
+        #####################################################
+        torchlike_mask = torchlike_data(mask)
+        #####################################################
 
-img = cv2.imread(image_file)
+        mask_list.append(torchlike_mask)
 
-cv2.imshow(f, img)
-cv2.waitKey(0)
+    mask_array = np.array(mask_list, dtype=np.int)
+    torch_mask = torch.from_numpy(mask_array).float()
+    if cuda:
+        torch_mask = torch_mask.cuda()
+    return torch_mask
+
+#####################################################
+def torchlike_data(data):
+    n_channels = data.shape[2]
+    torchlike_data = np.empty((n_channels, data.shape[0], data.shape[1]))
+    for ch in range(n_channels):
+        torchlike_data[ch] = data[:,:,ch]
+    return torchlike_data
+#####################################################
+
+def one_hot_encoder(data, n_class):
+    encoded_data = np.zeros((*data.shape, n_class), dtype=np.int) # (width, height, number_of_class)'lık bir array tanımlıyorum. 
+
+    encoded_labels = [[0,1], [1,0]]
+    for lbl in range(n_class):
+
+        encoded_label = encoded_labels[lbl] # lbl = 0 için (arkaplan) [1, 0] labelini oluşturuyorum, 
+                                # lbl = 1 için (freespace) [0, 1] labelini oluşturuyorum.
+        numerical_class_inds = data[:,:] == lbl # lbl = 0 için data'nın 0'a eşit olduğu w,h ikililerini alıyorum diyelim ki (F).
+                                                # lbl = 1 için data'nın 1'e eşit olduğu w,h ikililerini alıyorum diyelim ki (O).
+        encoded_data[numerical_class_inds] = encoded_label # lbl = 0 için tüm F'in sahip olduğu tüm w,h ikililerini [1, 0]'a eşitliyorum.
+                                                            # lbl = 1 için tüm O'un sahip olduğu tüm w,h ikililerini [0, 1]'e eşitliyorum.
+    return encoded_data
+
+####
+def image_mask_check(image_path_list, mask_path_list):
+    for image_path, mask_path in zip(image_path_list, mask_path_list):
+        image_name = image_path.split('/')[-1].split('.')[0]
+        mask_name  = mask_path.split('/')[-1].split('.')[0]
+        assert image_name == mask_name, "Image and mask name does not match {} - {}".format(image_name, mask_name)
 
 
+if __name__ == '__main__':
 
-def tensorize_image(image_path , output_shape):
-    for img in os.listdir('../data/images'):
-    im = Image.open(directory+img)
-    im_resized = im.resize(size, Image.ANTIALIAS)
-    im_resized.save(directory+img)
-    pass
 
-def decode_img(img):
-  # convert the compressed string to a 3D uint8 tensor
-  img = tf.image.decode_jpeg(img, channels=3)
-  # resize the image to the desired size
-  return tf.image.resize(img, [img_height, img_width])
+    image_list = glob.glob('../data/images/*')
+    image_list.sort()
+    batch_image_list = image_list[:4]
+    batch_image_tensor = tensorize_image(batch_image_list, (224, 224))
 
-#image_path, list of strings (paths: ["data/images/img1.png", .., "data/images/imgn.png"] corresponds to n images to be trained each step)
-#output_shape, list of integers (shape = (n1, n2): n1, n2 is width and height of the DNN model's input)
 
-#The tensor should be in [batch_size, output_shape[0], output_shape[1], 3] shape.
+    print(batch_image_list)
+    print(batch_image_tensor.dtype)
+    print(type(batch_image_tensor))
+    print(batch_image_tensor.shape)
 
-tensorize_mask(mask_path, output_shape):
 
-tensorize_mask [batch_size, width, height]
+    mask_list = glob.glob('../data/ex_masks/*')
+    mask_list.sort()
+    batch_mask_list = mask_list[:4]
+    batch_mask_tensor = tensorize_mask(batch_mask_list, (224, 224), 2)
 
-encoder = OneHotEncoder()
-encoder.fit(copy_mask)
 
-torch.Tensor()
-
-batch_masks = []
-
-batch_masks.append(copy_mask)
-
-batch_masks = np.array(batch_masks)
-
-mask_tensor = torch.Tensor(batch_masks)
-
-print(mask_tensor.shape)
-
-#The tensor should be in [batch_size, output_shape[0], output_shape[1], 2] shape.
-
-#Your model will accept the input with [batch_size, output_shape[0], output_shape[1], 3] shape and the label with [batch_size, output_shape[0], output_shape[1], 2] shape.
-
+    print(batch_mask_list)
+    print(batch_mask_tensor.dtype)
+    print(type(batch_mask_tensor))
+    print(batch_mask_tensor.shape)  
